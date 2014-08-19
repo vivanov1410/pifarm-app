b8baf728595a5b72dd2220a88aa07604aab69809
Svetlana Ivanova <sveta.Ivanova@fyidoctors.com>
2014-08-18 21:38:44 -0700

    created base project

---
+++ client/app/main/main.js
@@ -0,0 +1,11 @@
+'use strict';
+
+angular.module('pifarmAppApp')
+  .config(function ($stateProvider) {
+    $stateProvider
+      .state('main', {
+        url: '/',
+        templateUrl: 'app/main/main.html',
+        controller: 'MainCtrl'
+      });
+  });
