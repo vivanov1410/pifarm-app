ab1453a418f68f696dc5a84578f7cb104fa9c136
Svetlana Ivanova <sveta.Ivanova@fyidoctors.com>
2014-08-29 19:35:34 -0700

    added style for signup page

--- client/app/signup/signup.controller.js
+++ client/app/signup/signup.controller.js
@@ -4,7 +4,21 @@ angular.module('pifarm.app')
 .controller('SignupCtrl',
 ['$scope', 'Settings',
 function ($scope, Settings) {
-  
-  $scope.currentYear = Settings.currentYear;
+
+	$scope.account = {};
+
+  $scope.createAccount = function (account, form) {
+		console.log(form);
+
+		if(form.fullName.$error.minlength) {
+			$scope.fullNameLengthError = true;
+		}
+
+		if(form.$valid) {
+			$scope.loading = true;
+			$scope.createButtonText = 'Creating Account...';
+	  	console.log(account);
+  	}
+  };
 
 }]);
